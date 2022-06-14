f084d34a69e216ef4ddaae804c08a5b3bda0faff
mohashpat <66125392+mohashpat@users.noreply.github.com>
2022-06-14 12:23:43 +0100

    Add files via upload

---
+++ All users last logon.ps1
@@ -0,0 +1,4 @@
+﻿Get-ADUser -Filter * -SearchBase "xxxxx" -ResultPageSize 0 -Property CN, Description, LastLogonTimestamp |
+    Select-Object -Property CN, Description, @{ n = "LastLogonDate"; e = { [datetime]::FromFileTime( $_.lastLogonTimestamp ) } } |
+    Sort-Object -Property CN, Description, LastLogonDate     |
+    Export-CSV -NoTypeInformation "C:\myfile.csv"
---
+++ Users OU Information.ps1
@@ -0,0 +1,4 @@
+﻿Get-ADUser -Filter * -SearchBase "xxxxx" -ResultPageSize 0 -Property CN, Description, LastLogonTimestamp |
+    Select-Object -Property DistinguishedName, Name, samaccountname, Description, Enabled, @{ n = "LastLogonDate"; e = { [datetime]::FromFileTime( $_.lastLogonTimestamp ) } } |
+    Sort-Object -Property DistinguishedName, Name, samaccountname, Description, Enabled, LastLogonDate     |
+    Export-CSV -NoTypeInformation "C:\xxx.csv"
